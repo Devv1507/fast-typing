6665b8c4b084fc1de8df797285b047d2877b61b5
Devv1507 <davide.valencia.v@gmail.com>
2025-03-15 03:00:54 -0500

    Removed unused import

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,7 +4,6 @@ import com.example.controller.GameController;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
