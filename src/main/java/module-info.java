5385031437b25160aeeab83c5c122ce2ad7b83d7
Devv1507 <davide.valencia.v@gmail.com>
2025-03-15 21:23:22 -0500

    Added controller to module-info to avoid conflicts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
-module org.example.demo {
+module com.example {
     requires javafx.controls;
     requires javafx.fxml;
 
 
     opens com.example to javafx.fxml;
+    opens com.example.controller to javafx.fxml;
     exports com.example;
 }
